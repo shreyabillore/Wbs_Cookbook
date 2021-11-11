bded98d04c5c756b27ed4700c8309b363d1fe22e
Shreya Billore <shreyabillore410@gmail.com>
2021-11-11 20:53:41 +0100

    first commit

---
+++ src/client.js
@@ -0,0 +1,7 @@
+import * as contentful from 'contentful'
+
+export const client = contentful.createClient({
+    space: '91li6v3liq5k',
+    accessToken: 'dJq-U9aO3lRftDXqHJIHRwL-q38cD1WUd0bt-jpNux0',
+   
+})
